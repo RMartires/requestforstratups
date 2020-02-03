0c5f5df4070ea3ef5367a701dd60aae4dc2c5e4f
RMartires <rohitmartires14@gmail.com>
2020-02-03 08:09:49 +0530

    fixed signup but login giving issues

--- node_app.js
+++ node_app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const path = require('path');
+
 const app = express();
 
 const authRoute = require('./routes/auth');
+
+app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const authControler = require('../controllers/auth');
 
-router.get('/signup', authControler.postSignup);
+router.post('/signup', authControler.postSignup);
+
+router.post('/login', authControler.postLogin)
 
 module.exports = router;
